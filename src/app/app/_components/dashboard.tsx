78c5681531a8babdf4814f6d96fbe798fdf69c92
Lucas Carregal <lucascarregal123@gmail.com>
2024-08-11 15:50:17 -0300

    payment confirm feat

--- src/app/app/_components/dashboard.tsx
+++ src/app/app/_components/dashboard.tsx
@@ -3,81 +3,168 @@
  * @see https://v0.dev/t/Cob0gOzAFHl
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
-import { PrismaClient, User } from "@prisma/client";
+"use client";
 
+import { useState } from "react";
 import IndicatorsBoxs from "./indicatorsBoxs";
-import InstallmentsList from "./installmentsList";
-import { auth } from "@/services/auth";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogHeader,
+  DialogTitle,
+  DialogContent,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+import { FaWhatsapp } from "react-icons/fa";
+import { formatDate, formatValue } from "@/utils/utils";
+import { useRouter } from "next/navigation";
 
-const prisma = new PrismaClient();
-
-const tomorrow = new Date();
-tomorrow.setDate(tomorrow.getDate() + 1);
-tomorrow.setHours(0, 0, 0, 0); // Zerar horas, minutos, segundos e milissegundos
-
-export default async function Dashboard({ user }: { user: any }) {
-  const session = await auth();
-
-  const installments = await prisma.installment.findMany({
-    where: {
-      client: {
-        userId: session?.user?.id,
-      },
-      dueDate: {
-        lt: tomorrow,
-      },
-      paid: false,
-    },
-    include: {
-      client: true,
-    },
-  });
-
-  const futureInstallments = await prisma.installment.findMany({
-    where: {
-      client: {
-        userId: session?.user?.id,
-      },
-      dueDate: {
-        gt: tomorrow,
-      },
-      paid: false,
-    },
-  });
-
-  const hydratedInstallments = installments.map((installment) => {
-    return {
-      clientName: installment.client.name,
-      amount: installment.amount,
-      dueDate: installment.dueDate,
-      link:
-        "https://api.whatsapp.com/send?phone=55" +
-        installment.client.phone +
-        "&text=Ola",
-    };
-  });
+export default function Dashboard({
+  user,
+  installments,
+  futureInstallments,
+  hydratedInstallments,
+  confirmPayment,
+}: any) {
+  const router = useRouter();
 
   const hasPendingInstallments = hydratedInstallments.length !== 0;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [installmentConfirm, setInstallmentConfirm] = useState({
+    amount: 0,
+    client: "",
+    id: 0,
+  });
+
+  const handleConfirmPayment = () => {
+    confirmPayment(installmentConfirm.id);
+    router.refresh();
+  };
 
   return (
     <div className="flex flex-col w-full max-w-6xl mx-auto px-4 md:px-6 py-8">
       <div className="mb-4 text-lg font-medium">Olá, {user.name}!</div>
       <IndicatorsBoxs
         installmentsNumber={installments.length}
-        installmentsAmount={installments.reduce((acc, e) => {
+        installmentsAmount={installments.reduce((acc: number, e: any) => {
           return acc + e.amount;
         }, 0)}
         futureInstallmentsNumber={futureInstallments.length}
-        futureInstallmentsAmount={futureInstallments.reduce((acc, e) => {
-          return acc + e.amount;
-        }, 0)}
+        futureInstallmentsAmount={futureInstallments.reduce(
+          (acc: number, e: any) => {
+            return acc + e.amount;
+          },
+          0
+        )}
       />
       <div className="mt-8 grid gap-4 md:gap-6">
-        <InstallmentsList
-          installments={hydratedInstallments}
-          hasPendingInstallments={hasPendingInstallments}
-        />
+        <Card>
+          <CardHeader>
+            <CardTitle>Cobranças da vencidas</CardTitle>
+            <CardDescription>
+              Uma lista com todas as cobranças que tem o vencimento nessa semana
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            {hasPendingInstallments ? (
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Cliente</TableHead>
+                    <TableHead>Dia pagamento</TableHead>
+                    <TableHead>Valor</TableHead>
+                    <TableHead>Link WhatsApp</TableHead>
+                    <TableHead>Status</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {hydratedInstallments.map((installment: any, key: any) => {
+                    return (
+                      <TableRow key={key}>
+                        <TableCell>
+                          <div className="flex items-center gap-2">
+                            <div>{installment.clientName}</div>
+                          </div>
+                        </TableCell>
+                        <TableCell>{formatDate(installment.dueDate)}</TableCell>
+                        <TableCell>{formatValue(installment.amount)}</TableCell>
+                        <TableCell>
+                          <Button>
+                            <Link href={installment.link} target="_blank">
+                              <FaWhatsapp size={30} target="_blank" />
+                            </Link>
+                          </Button>
+                        </TableCell>
+                        <TableCell>
+                          <Button
+                            onClick={() => {
+                              setInstallmentConfirm({
+                                client: installment.clientName,
+                                amount: installment.amount,
+                                id: installment.id,
+                              });
+                              setIsModalOpen(true);
+                            }}
+                          >
+                            Pago
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            ) : (
+              <div className="flex items-center justify-center p-8 text-muted-foreground">
+                Não há cobranças pendentes.
+              </div>
+            )}
+          </CardContent>
+        </Card>
       </div>
+
+      <Dialog open={isModalOpen}>
+        <DialogTrigger asChild />
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Confirmar Pagamento</DialogTitle>
+          </DialogHeader>
+          <div className="space-y-4">
+            <p>
+              Você tem certeza que deseja confirmar o pagamento de{" "}
+              <strong>{formatValue(installmentConfirm?.amount)}</strong> de{" "}
+              <strong>{installmentConfirm.client}</strong>
+            </p>
+            <div className="flex justify-end gap-2">
+              <Button
+                variant="outline"
+                onClick={() => {
+                  setIsModalOpen(false);
+                }}
+              >
+                Cancelar
+              </Button>
+              <Button onClick={handleConfirmPayment}>Confirmar</Button>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,13 +1,100 @@
-import { auth, signOut } from "@/services/auth";
 import Dashboard from "./_components/dashboard";
+import { PrismaClient } from "@prisma/client";
+import { auth, signOut } from "@/services/auth";
+import { NextResponse } from "next/server";
+import { getUrl } from "@/lib/get-url";
+import { formatDate, formatValue } from "@/utils/utils";
+
+const prisma = new PrismaClient();
 
 export default async function Page() {
   const session = await auth();
 
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  tomorrow.setHours(0, 0, 0, 0);
+
   const logoff = async () => {
-    "use server";
     await signOut();
   };
 
-  return <Dashboard user={session?.user} />;
+  const installments = await prisma.installment.findMany({
+    where: {
+      client: {
+        userId: session?.user?.id,
+      },
+      dueDate: {
+        lt: tomorrow,
+      },
+      paid: false,
+    },
+    include: {
+      client: true,
+    },
+  });
+
+  const futureInstallments = await prisma.installment.findMany({
+    where: {
+      client: {
+        userId: session?.user?.id,
+      },
+      dueDate: {
+        gt: tomorrow,
+      },
+      paid: false,
+    },
+  });
+
+  const hydratedInstallments = installments.map((installment) => {
+    return {
+      id: installment.id,
+      clientName: installment.client.name,
+      amount: installment.amount,
+      dueDate: installment.dueDate,
+      link:
+        "https://api.whatsapp.com/send?phone=55" +
+        installment.client.phone +
+        "&text=" +
+        "Olá, " +
+        installment.client.name +
+        " a parcela do dia " +
+        formatDate(installment.dueDate) +
+        " no valor de " +
+        formatValue(installment.amount) +
+        " venceu!",
+    };
+  });
+
+  const confirmPayment = async (id: number) => {
+    "use server";
+    let updateInstallment = await prisma.installment.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (updateInstallment) {
+      updateInstallment.paid = true;
+      updateInstallment.payDate = new Date();
+
+      await prisma.installment.update({
+        where: {
+          id: id,
+        },
+        data: updateInstallment,
+      });
+    }
+
+    return NextResponse.redirect(new URL(getUrl("/app")));
+  };
+
+  return (
+    <Dashboard
+      user={session?.user}
+      installments={installments}
+      futureInstallments={futureInstallments}
+      hydratedInstallments={hydratedInstallments}
+      confirmPayment={confirmPayment}
+    />
+  );
 }
